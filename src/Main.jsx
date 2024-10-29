3e48e006e0a18e666210c35e3c3f68cf4212bba9
seoungjun02 <sjsj1072@gmail.com>
2024-10-29 21:52:03 +0900

    Add files via upload

---
+++ src/Main.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import './test.css';
+
+
+const Main  = () => {
+    return(
+        <div className="all">
+
+            <div className="Header">
+            <div className="nav"></div>
+            </div>    
+            
+           
+            <div className="section1">
+            <div className="article">
+            <div className="se1"></div>
+            <div className="se2"></div>
+            </div>
+            
+          
+
+
+          
+            <div className="article2">
+            <div className="se3"></div>
+            <div className="se4"></div>
+            </div>
+            
+            </div>
+            <div className="aside"></div>
+            <div className="footer">
+                <div className="address"></div>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Main;
